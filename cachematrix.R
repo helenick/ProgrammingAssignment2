1b3405395d3810a51684f3003740c820e7a65942
helenick <epyshkina@gmail.com>
2014-07-27 15:05:59 +0400

    helenick's solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+## Define set operation for matrix
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+## Define get operation for matrix
+        get <- function() x
+		
+##  Define get and set operation's for cached matrix
+        setinv <- function(inv) m <<- inv
+        getinv <- function() m
+## Compose object structure
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+## Check if matrix already computed and cashed
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+##set value for cashed matrix
+        x$setSolve(m)
+        m
 }
+
+        
